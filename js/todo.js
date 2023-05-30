b986691132649bad7d81bd4819a5abf3669b1dab
paquantum <enmw34@naver.com>
2023-05-30 22:08:06 +0900

    save toDos in localstorage

--- js/todo.js
+++ js/todo.js
@@ -2,14 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos() {
+    localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
 function deleteToDo(event) {
-    // 속성을 자세히 보면 버튼의 부모가 누구인지 알 수 있음
-    // console.dir(event.target.parentElement.innerText);
     const li = event.target.parentElement;
     li.remove();
 }
 
 function paintTodo(newTodo) {
+    
     const li = document.createElement("li");
     const span = document.createElement("span");
     span.innerText = newTodo;
@@ -25,7 +30,9 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintTodo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
